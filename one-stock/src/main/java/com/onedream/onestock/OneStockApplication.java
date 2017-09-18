2ecc36f33de8d612f2ecacc545701dd8ba83a533
ryan <luoxu2011@gmail.com>
2017-09-18 23:43:43 +0800

    Create spring starter (spring boot) project for one stock

---
+++ one-stock/src/main/java/com/onedream/onestock/OneStockApplication.java
@@ -0,0 +1,12 @@
+package com.onedream.onestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneStockApplication.class, args);
+	}
+}
